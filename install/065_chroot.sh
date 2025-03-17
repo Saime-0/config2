6e3c775f4ac87b89e5c912aa04bf99de69ce8479
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 02:17:54 +0300

    изменить chroot

--- install/065_chroot.sh
+++ install/065_chroot.sh
@@ -7,4 +7,5 @@ scripts_dir=/mnt$chroot_scripts_dir
 mkdir -p $scripts_dir
 parent_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cp $parent_dir/065_chroot/* $scripts_dir
-arch-chroot /mnt bash -c "sh ${chroot_scripts_dir}/*"
+arch-chroot /mnt bash -c "chmod +x ${chroot_scripts_dir}/*"
+arch-chroot /mnt bash -c "${chroot_scripts_dir}/*"
--- install/065_chroot/100_grub.sh
+++ install/065_chroot/100_grub.sh
@@ -3,7 +3,7 @@
 set -e
 
 # grub
-pacman -S grub efibootmgr
+pacman -S grub efibootmgr --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
