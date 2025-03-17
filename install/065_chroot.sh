b7fa2c21a86559a74a5317904adfdc94a9f3e127
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 01:30:28 +0300

    изменить 065 скрипт

--- install/065_chroot.sh
+++ install/065_chroot.sh
@@ -2,8 +2,18 @@
 
 set -e
 
-scripts_dir="/mnt/tmp/install$(date +%s)"
-mkdir $scripts_dir
+scripts_dir="/tmp/install$(date +%s)"
+mkdir /mnt/$scripts_dir
 parent_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-cp $parent_dir/065_chroot/* $scripts_dir
-arch-chroot /mnt /bin/bash -c ${scripts_dir}/*
+cp $parent_dir/065_chroot/* /mnt/$scripts_dir
+
+for script in $(ls *.sh); do
+    echo "Running the script: $script"
+    arch-chroot /mnt /bin/bash -c $scripts_dir/$script
+    # bash $scripts_dir/$script
+    # Проверка кода завершения
+    if [ $? -ne 0 ]; then
+        echo "Error: the $script script failed with an error. Execution stopped."
+        exit 1
+    fi
+done
