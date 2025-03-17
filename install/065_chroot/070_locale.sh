15b4c6f7d48787309b0dc846932fff6c3294218b
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 02:23:45 +0300

    исправить локаль

--- install/065_chroot/070_locale.sh
+++ install/065_chroot/070_locale.sh
@@ -3,7 +3,10 @@
 set -e
 
 # locale
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-echo "ru_RU.UTF-8 UTF-8" > /etc/locale.gen
+echo "
+en_US.UTF-8 UTF-8
+ru_RU.UTF-8 UTF-8
+" >> /etc/locale.gen
+
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
