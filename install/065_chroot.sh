d5184c085e82a047faf1a3413c1ecaf79bbe470a
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 01:47:43 +0300

    исправить путь к скриптам

--- install/065_chroot.sh
+++ install/065_chroot.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-scripts_dir="/mnt/root/tmp/install$(date +%s)"
+chroot_scripts_dir=/root/tmp/install$(date +%s)
+scripts_dir=/mnt$chroot_scripts_dir
 mkdir $scripts_dir
 parent_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cp $parent_dir/065_chroot/* $scripts_dir
-arch-chroot /mnt bash -c "${scripts_dir}/*"
+arch-chroot /mnt bash -c "${chroot_scripts_dir}/*"
