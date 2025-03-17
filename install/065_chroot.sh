69b567ce1e11b9bbf8a120f2e60a26f6ea004f90
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 01:19:28 +0300

    относительно родительской директории

--- install/065_chroot.sh
+++ install/065_chroot.sh
@@ -4,5 +4,6 @@ set -e
 
 scripts_dir="/mnt/tmp/install$(date +%s)"
 mkdir $scripts_dir
-cp ./065_chroot/* $scripts_dir
+parent_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+cp $parent_dir/065_chroot/* $scripts_dir
 arch-chroot /mnt ${scripts_dir}/*
