d4b2610e29aa7611df0f12cdbe20b4d38863651b
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 00:33:54 +0300

    конкатениция

--- install.sh
+++ install.sh
@@ -6,16 +6,24 @@ STEPS_DIR="install-by-step"
 # Переход в директорию с шагами
 cd "$STEPS_DIR" || { echo "Error: The $STEPS_DIR directory was not found."; exit 1; }
 
+gen_script=""
+
 # Проход по всем файлам в директории, отсортированным по имени
-for script in $(ls -v *.sh); do
-    echo "Running the script: $script"
-    # Выполнение скрипта
-    bash "$script"
+for script_file in $(ls -v *.sh); do
+    script=$(cat $script)
+    gen_script="
+    ${gen_script}
+    echo "Running the script: $script_file"
+    ${script}
     # Проверка кода завершения
     if [ $? -ne 0 ]; then
-        echo "Error: the $script script failed with an error. Execution stopped."
+        echo "Error: the $script_file script failed with an error. Execution stopped."
         exit 1
     fi
+    "
 done
+
+# Выполнение скрипта
+bash "$gen_script"
 
 echo "All the steps are completed successfully."
