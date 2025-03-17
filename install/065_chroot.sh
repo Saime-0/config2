9f36b48ce59b7d6d343d152f3ddc73e9acc196cd
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 01:49:18 +0300

    создавать промежуточные директори

--- install/065_chroot.sh
+++ install/065_chroot.sh
@@ -4,7 +4,7 @@ set -e
 
 chroot_scripts_dir=/root/tmp/install$(date +%s)
 scripts_dir=/mnt$chroot_scripts_dir
-mkdir $scripts_dir
+mkdir -p $scripts_dir
 parent_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cp $parent_dir/065_chroot/* $scripts_dir
 arch-chroot /mnt bash -c "${chroot_scripts_dir}/*"
