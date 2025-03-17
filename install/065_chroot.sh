4e2d621ff6027feba39c414ed154e19cde0d10d7
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 02:19:54 +0300

    изменить chroot

--- install/065_chroot.sh
+++ install/065_chroot.sh
@@ -2,10 +2,20 @@
 
 set -e
 
+# Создаем временную директорию для скриптов
 chroot_scripts_dir=/root/tmp/install$(date +%s)
 scripts_dir=/mnt$chroot_scripts_dir
 mkdir -p $scripts_dir
+
+# Копируем скрипты из родительской директории
 parent_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cp $parent_dir/065_chroot/* $scripts_dir
+
+# Делаем скрипты исполняемыми
 arch-chroot /mnt bash -c "chmod +x ${chroot_scripts_dir}/*"
-arch-chroot /mnt bash -c "${chroot_scripts_dir}/*"
+
+# Запускаем все скрипты по очереди
+for script in $(arch-chroot /mnt bash -c "ls ${chroot_scripts_dir}"); do
+    echo "Running script: ${script}"
+    arch-chroot /mnt bash -c "${chroot_scripts_dir}/${script}"
+done
