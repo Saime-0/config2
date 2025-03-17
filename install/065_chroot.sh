7a57da81d7b6d026f0f26c3f87a8db257b903ac2
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 01:20:53 +0300

    исправтиь команду arch-chroot

--- install/065_chroot.sh
+++ install/065_chroot.sh
@@ -6,4 +6,4 @@ scripts_dir="/mnt/tmp/install$(date +%s)"
 mkdir $scripts_dir
 parent_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cp $parent_dir/065_chroot/* $scripts_dir
-arch-chroot /mnt ${scripts_dir}/*
+arch-chroot /mnt /bit/bash -c "${scripts_dir}/*"
