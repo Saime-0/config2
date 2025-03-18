0021f8794250c1260c89de2c6ed09dadb6bc7f48
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 16:49:40 +0300

    опечатка в пути

--- install/055_format_and_mount.sh
+++ install/055_format_and_mount.sh
@@ -19,7 +19,7 @@ parted --script ${DEVICE} -- set 1 boot on
 
 echo "Formatting partitions ..."
 mkfs.fat -F 32 ${DEVICE}1  # EFI раздел
-fatlabel /dev/${DEVICE}1 BOOT
+fatlabel ${DEVICE}1 BOOT
 mkfs.ext4 ${DEVICE}2 -L ROOT      # Root раздел
 
 echo "Mounting partitions ..."
