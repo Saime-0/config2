e74d02472165aa9ba1ad71b79e67fe3da48f72e2
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-18 17:17:34 +0300

    добавить редактирование sudoer

--- install/065_chroot/104_user.sh
+++ install/065_chroot/104_user.sh
@@ -3,4 +3,5 @@
 set -e
 login=okg0
 useradd -m -G wheel $login
+echo "%wheel  ALL=(ALL:ALL)    NOPASSWD:SETENV: ALL" > /etc/sudoers.d/wheel
 passwd $login
--- install/069_apps.sh
+++ install/069_apps.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-pacman -Sy git networkmanager python sudo --noconfirm --needed --sysroot /mnt
+pacman -Sy git vim networkmanager python sudo --noconfirm --needed --sysroot /mnt
+
+arch-chroot /mnt /bash -c "systemctl enable --now NetworkManager.service"
