13a0f6e08a8d3f4a5563596a269f137a287eec22
Saime Dejuwyu <acc.saime.d@gmail.com>
2025-03-26 19:30:09 +0300

    добавить конфиг libfm

--- pkgs/pcmanfm.py
+++ pkgs/pcmanfm.py
@@ -4,6 +4,10 @@ import shutil
 script_parent = os.path.dirname(os.path.abspath(__file__))
 config_src = os.path.join(script_parent, "../dotfiles/.config/pcmanfm")
 config_dst = os.path.expanduser("~/.config/pcmanfm")
+# libfm:
+libfm_config_src = os.path.join(script_parent, "../dotfiles/.config/libfm")
+libfm_config_dst = os.path.expanduser("~/.config/libfm")
+
 
 class pcmanfm:
     pkg_name = "pcmanfm"
@@ -11,4 +15,5 @@ class pcmanfm:
     def setup():
         # Скопировать конфиг
         shutil.copytree(config_src, config_dst, dirs_exist_ok=True)
+        shutil.copytree(libfm_config_src, libfm_config_dst, dirs_exist_ok=True)
         
